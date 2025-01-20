556746ba1c7f8c19c58583710b56f2a60b8be532
Camilo Veríssimo Garcia Prado <camilovgprado21@gmail.com>
2025-01-20 18:53:30 -0300

    Mudanca de Pasta e Arquivos

---
+++ PreProcessamento de Dados/Dados_Censo.py
@@ -0,0 +1,4 @@
+from sklearn.preprocessing import StandardScaler
+import kagglehub, os, pandas as pd, numpy as np, seaborn as sns, matplotlib.pyplot as plt, plotly.express as px
+
+var_CSVCensus = pd.read_csv('Bases de dados\census.csv')
